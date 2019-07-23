1afd0fc7ee62915fb5417c61d507ec99327f5018
cerise612 <developerdanielle12@gmail.com>
2019-07-23 14:56:08 -0700

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// navigation
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -8,3 +9,36 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+function localStorageSupported() {
+  try {
+   return "localStorage" in window && window["localStorage"] !== null;
+  } catch (e) {
+   return false;
+  }
+ }
+
+// dark mode
+var checkbox = document.querySelector('input[name=theme]');
+checkbox.addEventListener('change', function() {
+  if(this.checked) {
+      trans()
+      document.documentElement.setAttribute('data-theme', 'dark'),
+      localStorage.setItem('theme', 'dark')
+  } else {
+      trans()
+      document.documentElement.setAttribute('data-theme', 'light'),
+      localStorage.setItem('theme', 'light')
+  }
+})
+
+let trans = () => {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+      document.documentElement.classList.remove('transition')
+  }, 1000)
+}
+console.log('which theme am I...' + localStorage.getItem('theme'))
+// store user preference
+// var data = localStorage.getItem('theme');
+
